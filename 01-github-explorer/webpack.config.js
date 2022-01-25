0990b3a6d8445cddf9707005a35b63c92b57667d
dekopossas <andrepossaster@gmail.com>
2022-01-24 22:18:53 -0300

    webpack.js

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, ''),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bunble.js',
