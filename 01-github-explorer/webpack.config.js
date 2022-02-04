8a68753442c13ef3f022d7a537fb0a2367768e95
dekopossas <andrepossaster@gmail.com>
2022-02-03 22:44:55 -0300

    rules done, this is awesome

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -30,6 +30,16 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
