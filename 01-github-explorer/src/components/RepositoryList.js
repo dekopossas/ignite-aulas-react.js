e7e7f64b71bbfceb6b0dfa54890744c545b216db
dekopossas <andrepossaster@gmail.com>
2022-02-06 23:49:51 -0300

    Repository

---
+++ 01-github-explorer/src/components/RepositoryList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function RepositoryList() {
+  return (
+    <div className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositórios</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default RepositoryList;
