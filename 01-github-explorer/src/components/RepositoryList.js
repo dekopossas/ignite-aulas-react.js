3125fea444c322942d58462ecd8f47c787dbab5d
DekoPossas <andrepossaster@gmail.com>
2022-02-19 13:01:49 -0300

    state done

--- 01-github-explorer/src/components/RepositoryList.js
+++ 01-github-explorer/src/components/RepositoryList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RepositoryItem from './RepositoryItem';
+import Counter from './Counter';
 
 function RepositoryList() {
   return (
@@ -7,11 +8,12 @@ function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={"Unform2"} />
-        <RepositoryItem repository={"Unform3"}/>
-        <RepositoryItem repository={"Unform5"} />
-        <RepositoryItem repository={"Unform6"} />
-        <RepositoryItem repository={"Unform7"} />
+        <RepositoryItem repository={'Unform2'} />
+        <RepositoryItem repository={'Unform3'} />
+        <RepositoryItem repository={'Unform5'} />
+        <RepositoryItem repository={'Unform6'} />
+        <RepositoryItem repository={'Unform7'} />
+        <Counter />
       </ul>
     </div>
   );
