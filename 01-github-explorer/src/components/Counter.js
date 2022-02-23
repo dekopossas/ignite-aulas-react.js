62df4ecf0f862f6962ab6b2409b9b2ef28c76160
DekoPossas <andrepossaster@gmail.com>
2022-02-22 22:15:59 -0300

    fast refresh

--- 01-github-explorer/src/components/Counter.js
+++ 01-github-explorer/src/components/Counter.js
@@ -11,7 +11,7 @@ function Counter() {
     <div>
       <h2>{counter}</h2>
       <button type="button" onClick={increment}>
-        Increment
+        Increment =#
       </button>
     </div>
   );
